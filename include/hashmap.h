2fd14715dcc23a98eee440f555f73653119da455
Alex Buckley <alexmeb81@outlook.com>
2025-11-26 02:18:51 -0800

    Implemented used bucket tracking for improved hashmap entry iteration

--- include/hashmap.h
+++ include/hashmap.h
@@ -13,40 +13,47 @@
 
 
 // Key-Value pair
-typedef struct hm_entry {
-    char *key;
-    int value;
-    struct hm_entry *next;
-} hm_entry;
+typedef struct HashMapEntry {
+    char *key; // Entry key
+    int value; // Entry value
+    struct HashMapEntry *next; // Pointer to next entry with hash-value
+} HashMapEntry;
 
 
 // Hashmap struct
 typedef struct {
-    hm_entry *buckets[HM_TABLE_SIZE];
-} hm_map;
+    HashMapEntry *buckets[HM_TABLE_SIZE]; // Array-based hashmap
+    int usedBuckets[HM_TABLE_SIZE]; // Tracks buckets with values
+    int usedCount; // Tracks number of used buckets
+} HashMap;
 
 
 // djb2 hashing function
-static inline unsigned long hm_hash(const char *str) {
-    unsigned long hash = 5381;
-    int c;
-    while ((c = *str++))
+static inline unsigned long hash(const char *str) {
+    unsigned long hash = 5381; // Initial hash value
+    int c; // Stores next char in key
+    
+    while((c = *str++)) // Apply hash function
         hash = ((hash << 5) + hash) + c;
+    
     return hash;
 }
 
 
 // Initialize empty hashmap
-static inline void hm_init(hm_map *m) {
+static inline void hashmapInit(HashMap *m) {
+    // Initialize all buckets to null
     for (int i = 0; i < HM_TABLE_SIZE; i++)
         m->buckets[i] = NULL;
+
+    m->usedCount = 0;
 }
 
 
 // Insert new value or update existing
-static inline void hm_put(hm_map *m, const char *key, int value) {
-    unsigned long h = hm_hash(key) % HM_TABLE_SIZE;
-    hm_entry *e = m->buckets[h];
+static inline void hashmapPut(HashMap *m, const char *key, int value) {
+    unsigned long h = hash(key) % HM_TABLE_SIZE;
+    HashMapEntry *e = m->buckets[h];
 
     while(e) { // Update existing key
         if (strcmp(e->key, key) == 0) {
@@ -56,8 +63,12 @@ static inline void hm_put(hm_map *m, const char *key, int value) {
         e = e->next;
     }
 
+    // Add bucket to used buckets if first entry
+    if (m->buckets[h] == NULL) 
+        m->usedBuckets[m->usedCount++] = h;
+
     // Create new entry
-    e = (hm_entry *)malloc(sizeof(hm_entry));
+    e = (HashMapEntry *)malloc(sizeof(HashMapEntry));
     e->key = strdup(key);
     e->value = value;
     e->next = m->buckets[h];
@@ -66,14 +77,14 @@ static inline void hm_put(hm_map *m, const char *key, int value) {
 
 
 // Retrieve value associated with key in hashmap
-static inline int hm_get(hm_map *m, const char *key, int *out_value) {
-    unsigned long h = hm_hash(key) % HM_TABLE_SIZE;
-    hm_entry *e = m->buckets[h];
+static inline int hashmapGet(HashMap *m, const char *key, int *outValue) {
+    unsigned long h = hash(key) % HM_TABLE_SIZE;
+    HashMapEntry *e = m->buckets[h];
 
-    while(e) { // Iterate through values at hash index
+    while(e) { // Iterate through bucket entries
         if (strcmp(e->key, key) == 0) { // Check if correct key
             // Key found
-            *out_value = e->value;
+            *outValue = e->value;
             return 1;
         }
         e = e->next;
@@ -83,12 +94,30 @@ static inline int hm_get(hm_map *m, const char *key, int *out_value) {
 }
 
 
+
+static inline void hashmapMap(HashMap *m, void (*fn)(const char *, int)) {
+    // Iterate through used buckets
+    for(int i = 0; i < m->usedCount; i++) {
+        int bucket = m->usedBuckets[i];
+        HashMapEntry *e = m->buckets[bucket];
+
+        while(e) { // Apply function to bucket entries
+            fn(e->key, e->value);
+            e = e->next;
+        }
+    }
+}
+
+
 // Deallocate hashmap memory
-static inline void hm_free(hm_map *m) {
-    for (int i = 0; i < HM_TABLE_SIZE; i++) {
-        hm_entry *e = m->buckets[i];
-        while (e) {
-            hm_entry *next = e->next;
+static inline void hashmapFree(HashMap *m) {
+    // Iterate through used buckets
+    for(int i = 0; i < m->usedCount; i++) {
+        int bucket = m->usedBuckets[i];
+        HashMapEntry *e = m->buckets[bucket];
+
+        while(e) { // Iterate bucket entries
+            HashMapEntry *next = e->next;
             free(e->key);
             free(e);
             e = next;
